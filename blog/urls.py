43e1d752e0540c0fa00cfa9ac1d430685d95bf2c
Lalchan Badsa <lalchanbadsa12345@gmail.com>
2021-11-13 10:34:58 +0600

    Django Social Blog

---
+++ blog/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import (
+    HomeView,
+    AboutView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+    LatestView,
+    CalendarView,
+    )
+
+urlpatterns = [
+    path('',HomeView.as_view(),name='home'),
+    path('user/<str:username>/',UserPostListView.as_view(),name='user_posts'),
+    path('about',AboutView.as_view(),name='about'),
+    path('post/<int:pk>/',PostDetailView.as_view(),name='detail'),
+    path('post/new/',PostCreateView.as_view(),name='post_create'),
+    path('post/<int:pk>/update/',PostUpdateView.as_view(),name='post_update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(),name='post_delete'),
+
+    path('post/latest/', LatestView.as_view(), name='latest_post'),
+    path('calendar', CalendarView.as_view(), name='calendar'),
+]
+
---
+++ blog/views.py
@@ -0,0 +1,85 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+from django.views.generic import (
+    View,
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+    )
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+
+# <app>/<model>_<viewtype>.html
+# defale tamplate location
+
+class HomeView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name = 'posts'
+    ordering=['-date_posted']
+    paginate_by = 5
+
+class UserPostListView(ListView):
+    model = Post
+    template_name='blog/user_posts.html'
+    context_object_name = 'posts'
+    paginate_by = 5
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+    
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name='blog/detail.html'
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    template_name='blog/post_create_form.html'
+    fields=['title','content']
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = Post
+    fields=['title','content']
+    # template_name='blog/post_form.html'
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    
+class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = Post
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+    success_url = '/'
+
+class LatestView(ListView):
+    model = Post
+    template_name='blog/latest_post.html'
+    context_object_name = 'posts'
+    ordering=['-date_posted']
+    paginate_by = 5    
+
+class CalendarView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'blog/calendar.html',)
+    
+    
+
+
+class AboutView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'blog/about.html',)
